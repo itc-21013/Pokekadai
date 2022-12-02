5c1cd188dd1a05bb29235a05c5529b430bfb8769
s21013 <s21013@std.it-college.ac.jp>
2022-12-02 11:45:28 +0900

    Inital commit

---
+++ app/src/main/java/jp/ac/it_college/std/s21013/whoisthatpokemon/MainActivity.kt
@@ -0,0 +1,19 @@
+package jp.ac.it_college.std.s21013.whoisthatpokemon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.ac.it_college.std.s21013.whoisthatpokemon.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        title = getString(R.string.app_name)
+        setContentView(binding.root)
+        initJsonData(assets)
+    }
+
+    override fun onBackPressed() {
+    }
+}
