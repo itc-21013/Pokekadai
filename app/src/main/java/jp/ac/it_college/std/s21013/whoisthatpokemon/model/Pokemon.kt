99f459234a11eb0a5f5c359e28ed0b8e45216288
s21013 <s21013@std.it-college.ac.jp>
2022-12-02 11:14:22 +0900

    データクラス追加

---
+++ app/src/main/java/jp/ac/it_college/std/s21013/whoisthatpokemon/model/Pokemon.kt
@@ -0,0 +1,11 @@
+package jp.ac.it_college.std.s21013.whoisthatpokemon.model
+
+data class PokemonJson (
+    val pokemon: List<Pokemon>
+)
+
+data class Pokemon(
+    val id: Int,
+    val name: String
+)
+
